560f7f6ee73afc5b6f67450cef5dba4d5218cb6b
kindaag <gabrielaalexandruu@gmail.com>
2020-03-17 13:33:07 +0100

    a2_multiclass_classification

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 # LT2212 V20 Assignment 2
 
-Put any documentation here including any answers to the questions in the 
-assignment on Canvas.
+Part 1
+After tokenization I lowercased and removed punctuation, empty strings, stop words. Only the words appearing over 10 times in the whole corpus have been taken into account. 
+In order to optimize the performance of the model I used tf-idf instead of the word counts.
+
+Part 4
+1.model_id 1 SGDClassifier
+Accuracy, precision, recall, f-measure: 90%
+model_id 2 LinearSVC
+- Accuracy, precision, recall, f-measure: 92%
+
+2.Reduced dimensionality with TruncatedSVD
+SGD Classifier model_id 1
+Features reduced to 50%:
+- accuracy and precision: 89%, recall and F-measure: 88%
+Features reduced to 25%:
+- accuracy, precision: 88%, recall and f-measure: 87%
+Features reduced to 10% :
+- accuracy and precision: 88%, recall and f-measure: 87%
+Features reduced to 5%: 
+- accuracy and precision: 87%, recall and f-measure: 86%
+
+LinearSVC model_id 2
+Features reduced to 50%:
+- accuracy, precision, recall, f-measure: 92%
+Features reduced to 25%:
+- accuracy and precision: 93%, recall and f-measure: 92%
+Features reduced to 10%:
+- accuracy, precision, f-measure: 90%, recall: 89%
+Features reduced to 5%:
+- accuracy, precision, f-measure, recall: 89%
+
+When running the program using the SGD and the LinearSVC Classifiers by using the original space, but also by reducing the dimensions, I have noticed that there is little to almost no difference between the results obtained in the evaluation. After having written down these values, I tried running the program again and again and it has occured multiple times that the metrics indicated a higher performance of the classifiers when DR has been applied than when the original number of features was used.
+Having done some readings on dimensionality reduction and also observing my program's performance, DR definitely makes learning faster and has the benefit of increasing computational efficiency.
+
+p.s. I have written another function for part bonus but due to time constraints I haven't managed to execute the new implementation of part2 and report the results.
--- a2.py
+++ a2.py
@@ -4,7 +4,28 @@ from sklearn.datasets import fetch_20newsgroups
 from sklearn.base import is_classifier
 import numpy as np
 random.seed(42)
+from collections import Counter
+from nltk.tokenize import word_tokenize
+import string
+import pandas as pd
+from sklearn.decomposition import TruncatedSVD
+from scipy.sparse import random as sparse_random
+from sklearn.random_projection import sparse_random_matrix
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from sklearn.linear_model import SGDClassifier
+from nltk.corpus import stopwords 
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn import preprocessing
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 
+newsgroups_data = fetch_20newsgroups(subset='all', shuffle=True, random_state=42)
+data = newsgroups_data.data
+labels = newsgroups_data.target
 
 ###### PART 1
 #DONT CHANGE THIS FUNCTION
@@ -16,13 +37,40 @@ def part1(samples):
     print("Data shape: ", X.shape)
     return X
 
-
 def extract_features(samples):
-    print("Extracting features ...")
-    pass #Fill this in
+    l=[]
+    stop_words = set(stopwords.words('english')) 
+    for sample in samples:
+        sample = word_tokenize(sample)
+        sample = [''.join(c.lower() for c in s if c!="") for s in sample if s not in string.punctuation if s.isalpha() is True]
+        f_sample = [w for w in sample if not w in stop_words] 
+           
+        my_dict = dict(Counter(f_sample))
+        l.append(my_dict)
 
+    df = pd.DataFrame(l)
+    sum_column = df.sum(axis=0)
+    dictionary = sum_column[1:].to_dict()
 
+    d = {}
+    for key, value in dictionary.items():
+        if value > 10:
+            d[key] = value
 
+    final_dict = {}
+    for data_key in df.keys():
+        for k, v in d.items():
+            if data_key == k:
+                final_dict[data_key] = list(df[data_key])
+
+    df2 = pd.DataFrame(final_dict)
+    df2.fillna(0, inplace=True)
+    arr = np.array(df2)
+    tfidf_transformer=TfidfTransformer(smooth_idf=True,use_idf=True)
+    arr2 = tfidf_transformer.fit_transform(arr)
+
+    return arr2.toarray()
+    
 ##### PART 2
 #DONT CHANGE THIS FUNCTION
 def part2(X, n_dim):
@@ -35,20 +83,24 @@ def part2(X, n_dim):
     print("Dim reduced data shape: ", X_dr.shape)
     return X_dr
 
+def reduce_dim(X, n=2):
+    svd = TruncatedSVD(n)
+    transformed = svd.fit_transform(X)
+    return transformed
 
-def reduce_dim(X,n=10):
-    #fill this in
-    pass
-
+def reduce2(X, n=2):
+    pca = PCA()
+    X_reduced = pca.fit_transform(X)
+    return X_reduced
 
 
 ##### PART 3
 #DONT CHANGE THIS FUNCTION EXCEPT WHERE INSTRUCTED
 def get_classifier(clf_id):
     if clf_id == 1:
-        clf = "" # <--- REPLACE THIS WITH A SKLEARN MODEL
+        clf = SGDClassifier(loss='log', penalty='l2', random_state=42, max_iter=10000, tol=1e-3, average=True)
     elif clf_id == 2:
-        clf = "" # <--- REPLACE THIS WITH A SKLEARN MODEL
+        clf = LinearSVC(random_state=0, max_iter=10000)
     else:
         raise KeyError("No clf with id {}".format(clf_id))
 
@@ -57,6 +109,7 @@ def get_classifier(clf_id):
     return clf
 
 #DONT CHANGE THIS FUNCTION
+
 def part3(X, y, clf_id):
     #PART 3
     X_train, X_test, y_train, y_test = shuffle_split(X,y)
@@ -70,36 +123,46 @@ def part3(X, y, clf_id):
     print("Test example: ", X_test[0])
     print("Train label example: ",y_train[0])
     print("Test label example: ",y_test[0])
-    print()
-
 
     #train model
     print("Training classifier ...")
-    train_classifer(clf, X_train, y_train)
+    train_classifier(clf, X_train, y_train)
 
 
     # evalute model
-    print("Evaluating classcifier ...")
+    print("Evaluating classifier ...")
     evalute_classifier(clf, X_test, y_test)
 
 
 def shuffle_split(X,y):
-    pass # Fill in this
+    X, y = shuffle(X, y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
+    return X_train, X_test, y_train, y_test
 
-def train_classifer(clf, X, y):
+      
+def train_classifier(clf, X, y):
     assert is_classifier(clf)
-    ## fill in this
+    model = clf.fit(X, y)
+    return model
 
 
 def evalute_classifier(clf, X, y):
     assert is_classifier(clf)
-    #Fill this in
+    y_pred = clf.predict(X)
+    precision = precision_score(y,y_pred, average = "macro")
+    recall = recall_score(y,y_pred, average = "macro")
+    f_measure = f1_score(y,y_pred, average = "macro")
+    
+    print("Accuracy:", accuracy_score(y, y_pred, normalize=True))
+    print("Precision:", precision)
+    print("Recall:", recall)
+    print("F-measure:", f_measure)
 
-
+    
 ######
 #DONT CHANGE THIS FUNCTION
-def load_data():
+def load_data(data):
     print("------------Loading Data-----------")
     data = fetch_20newsgroups(subset='all', shuffle=True, random_state=42)
     print("Example data sample:\n\n", data.data[0])
@@ -108,14 +171,13 @@ def load_data():
     print("Number of possible labels: ", len(data.target_names))
     return data.data, data.target, data.target_names
 
-
 #DONT CHANGE THIS FUNCTION
 def main(model_id=None, n_dim=False):
 
     # load data
     samples, labels, label_names = load_data()
 
-
+    
     #PART 1
     print("\n------------PART 1-----------")
     X = part1(samples)
@@ -129,8 +191,6 @@ def main(model_id=None, n_dim=False):
     if model_id:
         print("\n------------PART 3-----------")
         part3(X, labels, model_id)
-
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
